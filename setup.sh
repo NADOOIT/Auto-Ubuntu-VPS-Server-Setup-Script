2818e2e1a29a4d22653ef18bfe37cb3d5b2c734f
Christoph Backhaus <106314951+NADOOITChristophBa@users.noreply.github.com>
2024-03-04 11:41:43 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,7 +29,6 @@ echo "This script will install Docker, Docker Compose, and set up Portainer and 
 read skip_to_service_install
 
 if [[ "$skip_to_service_install" =~ ^([yY][eE][sS]|[yY])*$ ]]; then
-then
     echo "Continuing with Docker, Portainer, and NGINX Proxy Manager setup."
 
     # Install Docker
